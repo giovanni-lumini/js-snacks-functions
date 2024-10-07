172a7b1a8213f331ceeca666c3efa25182fad04e
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-07 19:07:07 +0200

    saluti-complete

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,13 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
-
 // Dichiara la funzione qui.
-
+function hello_name(name) {
+    const hello=`ciao ${name}`;
+    return hello
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const name2=hello_name('Mario')
+console.log(name2);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
