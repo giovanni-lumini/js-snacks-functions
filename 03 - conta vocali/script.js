9bb71d379950071b8c31c27033db0f6e968c24cc
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-07 18:48:09 +0200

    conta-vocali-complete

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,9 +1,22 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+/* const word = 'javascript'; */
 
 
 // Dichiara la funzione qui.
+function vowels_inside_word(word) {
+    const vowels = 'aeiou';
+    let count = 0;
+    for(let i=0; i<word.length; i++){
+        if(vowels.includes(word[i])){
+            console.log(word[i]);
+            count++;
+        }             
+    }
+    return count;
+}
+
+console.log(vowels_inside_word("javascript"));""
 
 
 // Invoca la funzione qui e stampa il risultato in console
