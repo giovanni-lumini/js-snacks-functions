f4e228d68348ba2c52e3c39104eeb86e765e286e
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-07 17:15:07 +0200

    iniziali-complete

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+/* 
+1-inserire un array
+2-prendere ogni item dell'array
+3-prendere l'iniziale dell'item
+4-stampare tutte le iniziali degli item
+*/
 
 // Dichiara la funzione qui.
-
+function getInitials(names) {
+    return names.map(name => name[0]);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(getInitials(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
